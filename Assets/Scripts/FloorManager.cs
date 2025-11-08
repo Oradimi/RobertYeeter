c2cdb58637bbc772be05780ff6dec24ed9e2d1b3
Oradimi <loganargouse@gmail.com>
2025-11-08 18:09:36 +0100

    Can now control the player character

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -3,60 +3,73 @@ using UnityEngine;
 
 public class FloorManager : MonoBehaviour
 {
-    [SerializeField] private GameObject floorPrefab;
+    [SerializeField] private Transform floorPrefab;
+    [SerializeField] private Transform waterPrefab;
     [SerializeField] private float speed = 5f;
     
-    private List<GameObject> _floors;
-    private List<Vector3> _floorsPreviousPosition;
+    private List<Transform> _floor;
+    private List<Vector3> _floorPreviousPosition;
+    private List<Transform> _water;
+    private List<Vector3> _waterPreviousPosition;
     
     private void Awake()
     {
-        if (!floorPrefab)
+        if (!floorPrefab || !waterPrefab)
             return;
         
-        _floors = new List<GameObject>();
-        _floorsPreviousPosition = new List<Vector3>();
+        _floor = new List<Transform>();
+        _floorPreviousPosition = new List<Vector3>();
+        _water = new List<Transform>();
+        _waterPreviousPosition = new List<Vector3>();
 
         var children = GetComponentsInChildren<Transform>();
         foreach (var child in children)
             if (child != transform)
                 Destroy(child.gameObject);
         
-        _floors.Add(Instantiate(floorPrefab, transform));
-        _floorsPreviousPosition.Add(transform.localPosition);
+        _floor.Add(Instantiate(floorPrefab, transform));
+        _floorPreviousPosition.Add(transform.localPosition);
+        _water.Add(Instantiate(waterPrefab, transform));
+        _waterPreviousPosition.Add(transform.localPosition);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (!floorPrefab)
+        if (!floorPrefab || !waterPrefab)
             return;
+
+        UpdateScrollingEnvironment(floorPrefab, speed * 1.0f, _floor, _floorPreviousPosition);
+        UpdateScrollingEnvironment(waterPrefab, speed * 1.5f, _water, _waterPreviousPosition);
+    }
+
+    private void UpdateScrollingEnvironment(Transform prefab, float elementSpeed, List<Transform> element, List<Vector3> elementPreviousPosition)
+    {
+        var elementToAdd = false;
+        var elementToRemove = false;
+        for (var i = 0; i < element.Count; i++)
+        {
+            element[i].localPosition += Vector3.forward * (elementSpeed * Time.deltaTime);
+            
+            if (elementPreviousPosition[i].z <= 42f && element[i].localPosition.z > 42f)
+                elementToAdd = true;
+
+            if (elementPreviousPosition[i].z <= 84f && element[i].localPosition.z > 84f)
+                elementToRemove = true;
+            
+            elementPreviousPosition[i] = element[i].localPosition;
+        }
         
-        var floorToAdd = false;
-        var floorToRemove = false;
-        for (var i = 0; i < _floors.Count; i++)
+        if (elementToAdd)
         {
-            _floors[i].transform.localPosition += Vector3.forward * (speed * Time.deltaTime);
-            
-            if (_floorsPreviousPosition[i].z <= 42f && _floors[i].transform.localPosition.z > 42f)
-                floorToAdd = true;
-
-            if (_floorsPreviousPosition[i].z <= 84f && _floors[i].transform.localPosition.z > 84f)
-                floorToRemove = true;
-            
-            _floorsPreviousPosition[i] = _floors[i].transform.localPosition;
+            element.Add(Instantiate(prefab, elementPreviousPosition[0] + Vector3.back * 84f, Quaternion.identity, transform));
+            elementPreviousPosition.Add(elementPreviousPosition[0] + Vector3.back * 84f);
         }
 
-        if (floorToAdd)
+        if (elementToRemove)
         {
-            _floors.Add(Instantiate(floorPrefab, _floorsPreviousPosition[0] + Vector3.back * 84f, Quaternion.identity, transform));
-            _floorsPreviousPosition.Add(_floorsPreviousPosition[0] + Vector3.back * 84f);
-        }
-
-        if (floorToRemove)
-        {
-            Destroy(_floors[0]);
-            _floors.RemoveAt(0);
-            _floorsPreviousPosition.RemoveAt(0);
+            Destroy(element[0].gameObject);
+            element.RemoveAt(0);
+            elementPreviousPosition.RemoveAt(0);
         }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    
+    private PlayerControls _controls;
+    private Vector3 _targetPosition;
+    
+    private void Awake()
+    {
+        _controls = new PlayerControls();
+        _targetPosition = new Vector3(1, 0, 0);
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+        _controls.Player.Move.performed += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Move.performed -= OnMove;
+        _controls.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, speed * Time.fixedDeltaTime);
+    }
+    
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        var input = ctx.ReadValue<Vector2>();
+        Debug.Log(input);
+        _targetPosition = new Vector3(Mathf.Clamp(_targetPosition.x - input.x, -2.0f, 2.0f), 0, 0);
+    }
+}
